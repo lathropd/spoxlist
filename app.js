defd66f3cca035cd8b6e95a8842719f323bc5903
Daniel Lathrop <daniel.lathrop@gmail.com>
2019-03-29 13:25:49 -0500

    allow it run locally

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ const express = require('express')
 // create an app
 let app = express()
 app.get('/', function(req, res) {
-    res.send('<h1>Hello Herky</h1>')
+    res.send('Hello Herky')
 })
+
+
+
+// run server in development mode
+// let hostname = 'localhost'
+let port = 3000
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}/`);
+}) 
